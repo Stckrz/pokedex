0ed54463a0871869db82374f2e67ac8f3a54097d
Stckrz <rin449@gmail.com>
2024-02-18 19:42:34 -0600

    added my play file

---
+++ src/library/pokemonapi.js
@@ -0,0 +1,53 @@
+async function fetchPokemon() {
+
+	try {
+		let pokemonArray = []
+		const response = await fetch(`https://pokeapi.co/api/v2/pokemon/1`)
+		const fetchedData = await response.json();
+		console.log(fetchedData.sprites.front_default)
+		// fetchedData.results.map((item) => {
+		// 	item.name !== undefined &&
+		// 		pokemonArray.push(item);
+		// })
+		// if (pokemonArray.length > 0) {
+		// 	console.log(pokemonArray)
+		// 	return pokemonArray
+		// }
+		// else {
+		// 	return ([])
+		// }
+	} catch (error) { console.log(error) }
+}
+async function fetchPokemonSprites() {
+const response = await fetch('https://beta.pokeapi.co/graphql/v1beta', {
+  method: 'POST',
+
+  headers: {
+    "Content-Type": "application/json"
+  },
+
+  body: JSON.stringify({
+
+			query: `{
+  pokemon_v2_pokemonsprites {
+    pokemon_v2_pokemon {
+      name
+      pokemon_v2_pokemonsprites(distinct_on: id) {
+        sprites(path: "front_default")
+      }
+    }
+  }
+}`
+    // query: `{
+    //   characters {
+    //     results {
+    //       name
+    //     }
+    //   }
+    // }`
+  })
+})
+	const data = await response.json()
+	console.log(data.data.pokemon_v2_pokemonsprites[0])
+}
+fetchPokemonSprites()
